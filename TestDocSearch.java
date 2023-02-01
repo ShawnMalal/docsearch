eb26fa5f55701f8a606cd5c8c72c4f3493aee703
ShawnMalal <shawnmalal@gmail.com>
2023-02-01 14:39:23 -0800

    lab 4 updates

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.beans.Transient;
 import java.io.IOException;
 
 public class TestDocSearch {
@@ -14,9 +15,17 @@ public class TestDocSearch {
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
     Handler h = new Handler("./written_2/");
-    URI rootPath = new URI("http://localhost/search?q=sushi");
-    String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
-    assertEquals(expect, h.handleRequest(rootPath));
-	}
+    URI rootPath = new URI("http://localhost/search?q=hello");
+    String expect = "Found 2 paths: ./written_2/travel_guides/berlitz1/WhereToHongKong.txt ./written_2/travel_guides/berlitz1/WhereToItaly.txt";
+    }
+    @Test 
+	public void testSearch1() throws URISyntaxException, IOException {
+    Handler h = new Handler("./written_2/");
+    URI rootPath = new URI("http://localhost/search?q=");
+    String expect = "Found 4 paths: ./written_2/travel_guides/berlitz1/WhereToFrance.txt ./written_2/travel_guides/berlitz2/Budapest-WhereoGo.txt ./written_2/travel_guides/berlitz2/California-WhereToGo.txt ./written_2/travel_guides/berlitz2/Nepal-WhereToGo.txt";
 }
+}
+
+
+
 
